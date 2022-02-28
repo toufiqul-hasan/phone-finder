4e29a48a01bd10dc91d251884ce6e5b5bf42c734
toufiqul-hasan <toufiqul.shihab@gmail.com>
2022-03-01 01:00:46 +0600

    details result added

--- js/app.js
+++ js/app.js
@@ -22,10 +22,36 @@ const searchResult = (phones) => {
         <div class="card-body">
           <h5 class="card-title">${phone.phone_name}</h5>
           <p class="card-text">${phone.brand}</p>
-          <button onclick="cardDetails('${phone.code}')" class="btn btn-primary">See Details</button>
+          <button onclick="phoneDetails('${phone.slug}')" class="btn btn-primary">See Details</button>
         </div>
       </div>
     `;
     result.appendChild(div);
   }
+};
+// Handle Details Result
+const phoneDetails = (slug) => {
+  fetch(`https://openapi.programming-hero.com/api/phone/${slug}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const alldetails = data.data;
+      const singleDetails = alldetails;
+      const div = document.createElement("div");
+      div.classList.add("col-lg-4");
+      div.classList.add("mb-5");
+      div.innerHTML = `
+      <div class="card" style="width: 18rem;">
+        <img src="${singleDetails.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${singleDetails.name}</h5>
+          <p class="card-text">${singleDetails.releaseDate}</p>
+          <p class="card-text">${singleDetails.mainFeatures.storage}</p>
+          <p class="card-text">${singleDetails.mainFeatures.displaySize}</p>
+          <p class="card-text">${singleDetails.mainFeatures.chipSet}</p>
+          <p class="card-text">${singleDetails.mainFeatures.memory}</p>
+        </div>
+      </div>
+      `;
+      details.appendChild(div);
+    });
 };
